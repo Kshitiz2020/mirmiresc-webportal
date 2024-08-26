dc0ee85efaca3b76af109d068aeb522542508ed4
sushil.bastola@zalando.fi <suzilxptr@gmail.com>
2024-08-26 21:47:29 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./mirmire.jpg" className="App-logo" alt="logo" />
         <p>
           Under construction mirmire website
         </p>
