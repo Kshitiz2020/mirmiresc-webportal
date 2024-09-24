2c02fd7356a0b380502d610e523c9f545d7d4a3e
sushil.bastola@zalando.fi <suzilxptr@gmail.com>
2024-09-24 18:13:21 +0300

    all excel sheets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,11 @@ function App() {
 
       <body>
       <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=465633707&amp;single=true&amp;widget=true&amp;headers=false" title="Table" width="100%" height="400"></iframe>
-      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=524323757&single=true" title="Score board" width="100%" height="400"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=447411446&single=true&amp;single=true&amp;widget=true&amp;headers=false" title="Score board" width="100%" height="200"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=524323757&single=true&amp;single=true&amp;widget=true&amp;headers=false" title="Score board" width="100%" height="550"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=350534510&single=true&amp;single=true&amp;widget=true&amp;headers=false" title="Score board" width="100%" height="350"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=1735482426&single=true&amp;single=true&amp;widget=true&amp;headers=false" title="Score board" width="100%" height="350"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=446425707&single=true&amp;single=true&amp;widget=true&amp;headers=false" title="Score board" width="100%" height="450"></iframe>
       </body>
     </div>
   );
