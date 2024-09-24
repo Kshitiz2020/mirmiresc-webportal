3ea0fa590bd86509c0489d22c49b82930a5401eb
sushil.bastola@zalando.fi <suzilxptr@gmail.com>
2024-09-24 15:41:22 +0300

    iframe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ function App() {
         >
           Shout out to thulabaas
         </a>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=465633707&amp;single=true&amp;widget=true&amp;headers=false" title="Table"></iframe>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=524323757&single=true" title="Score board"></iframe>
       </header>
     </div>
   );
