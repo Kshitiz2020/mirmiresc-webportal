86c47efb387b0332079a1dfcca892a81820f0bcb
sushil.bastola@zalando.fi <suzilxptr@gmail.com>
2024-09-24 17:25:36 +0300

    excel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./mirmire.jpg" className="App-logo" alt="logo" />
-        <p>
-          Under construction mirmire website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Shout out to thulabaas
-        </a>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=465633707&amp;single=true&amp;widget=true&amp;headers=false" title="Table"></iframe>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=524323757&single=true" title="Score board"></iframe>
+    
+        
       </header>
+
+      <body>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=465633707&amp;single=true&amp;widget=true&amp;headers=false" title="Table" width="100%" height="400"></iframe>
+      <iframe class="iframe" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROZfyFerDBhhOYV8GRezFJ0Q4a1bInSSOvpZYZLGQiqTxXDZpCUJQxCX7bRkNOpSddTfX2HVPlyyJL/pubhtml?gid=524323757&single=true" title="Score board" width="100%" height="400"></iframe>
+      </body>
     </div>
   );
 }
